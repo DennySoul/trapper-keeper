f9c88d5475c30b7e1045eb7397c266f9eebf8888
Denis Chernysh <mr.denis.chernysh@gmail.com>
2018-07-20 13:22:01 +0300

    Dc workshop s3 solution (#1)
    * inited awsmobile
    
    * added work with S3 bucket CRUD files
    
    * added S3 private images param and Authenticator wrapper

--- src/Application.js
+++ src/Application.js
@@ -1,36 +1,87 @@
 import React, { Component } from 'react';
 import './Application.css';
 
-class Application extends Component {
-  state = {
-    files: []
-  };
+import { withAuthenticator } from 'aws-amplify-react';
+import { Storage } from 'aws-amplify';
 
-  handleSubmit = event => {
-    event.preventDefault();
+Storage.configure({ level: 'private' });
 
-    const file = this.fileInput.files[0];
-    const { name } = file;
+class S3Image extends Component {
+    state = { src: null };
 
-    console.log(file, name);
-  };
+    async componentDidMount() {
+        const src = await Storage.get(this.props.s3key, { expires: 10 });
+        this.setState((state, props) => ({ ...state, src }));
+    }
 
-  render() {
-    return (
-      <div className="Application">
-        <form className="NewItem" onSubmit={this.handleSubmit}>
-          <input
-            type="file"
-            ref={input => this.fileInput = input}
-          />
-          <input className="full-width" type="submit" />
-        </form>
-        <section className="Application-images">
+    render() {
 
-        </section>
-      </div>
-    );
-  }
+        if (!this.state.src)
+            return null;
+
+        return (
+            <article>
+                <img
+                    key={Math.random()}
+                    src={this.state.src}
+                    alt=""
+                    onClick={ this.props.handleRemove }
+                />
+            </article>
+        );
+    }
 }
 
-export default Application;
+class Application extends Component {
+    state = {
+        files: []
+    };
+
+    async componentDidMount() {
+        const files = await Storage.list('');
+        this.setState((state, props) => ({ ...state, files }));
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        const file = this.fileInput.files[0];
+        const { name } = file;
+
+        Storage.put(name, file)
+            .then(res =>
+                this.setState((state, props) => ({ files: [...state.files, res] }))
+            );
+    };
+
+    handleRemove = (fileKey) => (event) => {
+        event.preventDefault();
+        const files = this.state.files.filter((file) => file.key !== fileKey);
+
+        Storage.remove(fileKey)
+            .then(() => this.setState((state, props) => ({ files })));
+    };
+
+    render() {
+        return (
+            <div className="Application">
+                <form className="NewItem" onSubmit={this.handleSubmit}>
+                    <input
+                        type="file"
+                        ref={input => this.fileInput = input}
+                    />
+                    <input className="full-width" type="submit"/>
+                </form>
+                <section className="Application-images">
+                    {
+                        this.state.files.map((s3item) => (
+                            <S3Image key={s3item.key} s3key={s3item.key} handleRemove={this.handleRemove(s3item.key)}/>)
+                        )
+                    }
+                </section>
+            </div>
+        );
+    }
+}
+
+export default withAuthenticator(Application);
